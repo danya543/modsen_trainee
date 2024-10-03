d67cf59ab0a54848a17a460358ea5d085dbf22a4
Daniil <dgilev75@gmail.com>
2024-10-03 11:28:28 +0300

    fix:image error

--- museum/src/components/Logo/Logo.tsx
+++ museum/src/components/Logo/Logo.tsx
@@ -1,15 +1,18 @@
 import { useNavigate } from "react-router-dom";
 
+import BlackLogo from '~/assets/black_logo.svg'
+import LightLogo from '~/assets/logo.svg'
+
 export const LogoThemes = {
     Black: 'black_logo',
-    Light: 'logo'
+    Light: 'ligt_logo'
 } as const;
 
 export const Logo = ({ theme }: { theme: typeof LogoThemes[keyof typeof LogoThemes] }) => {
     const navigate = useNavigate();
     return (
         <div onClick={() => navigate('/')}>
-            <img style={{ width: '100%' }} src={`/src/assets/${theme}.svg`} alt="Logo" />
+            <img style={{ width: '100%' }} src={theme === LogoThemes.Black ? BlackLogo : LightLogo} alt="Logo" />
         </div>
     );
 };
